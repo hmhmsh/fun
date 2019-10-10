45c251eab644f6eda73fbb7105eee66a8877f78f
hase <s-hasegawa@infocity.co.jp>
2019-10-10 11:00:21 +0900

    static func for stub

--- Fun.playground/Contents.swift
+++ Fun.playground/Contents.swift
@@ -1,1 +1,29 @@
 
+protocol AProtocol {
+	static func a()
+}
+
+struct A: AProtocol {
+	static func a() {
+		print("A func a")
+	}
+}
+
+struct B<T: AProtocol> {
+	
+	func b() {
+		T.a()
+	}
+}
+
+let b = B<A>()
+b.b()
+
+struct AStub: AProtocol {
+	static func a() {
+		print("AStub func a")
+	}
+}
+
+let bStub = B<AStub>()
+bStub.b()
